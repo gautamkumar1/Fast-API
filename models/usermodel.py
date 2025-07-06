af04d92e08b7b504192141b894631e1751a3df54
gautamkumar1 <gautamraaz936@gmail.com>
2025-07-06 12:27:45 +0530

    created a register route

--- models/usermodel.py
+++ models/usermodel.py
@@ -4,8 +4,15 @@ class RegisterModel(BaseModel):
     username: str
     email: EmailStr
     password: str
+class LoginModel(BaseModel):
+    email: EmailStr
+    password: str
 
 class UserData(BaseModel):
     id: int
     username: str
     email: EmailStr
+class UserResponse(BaseModel):
+    user: UserData
+    access_token: str
+
